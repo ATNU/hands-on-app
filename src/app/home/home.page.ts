affbc8c4bfd35cc4bfb923793dac857d9941a0e6
KateCourt <nkc124@ncl.ac.uk>
2019-10-14 15:41:44 +0100

    ID passing between home and feedback pages

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import { UUID } from 'angular2-uuid';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  uniqueID: string;
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.uniqueID = UUID.UUID();
+    console.log(this.uniqueID);
+  }
+
+  feedbackClicked() {
+this.router.navigate(['/feedback/' + this.uniqueID]);
+  }
 
 }
