6a18a4538b0a2570242520f5d3f2bf61ba16f75e
Fiona Galston <fiona.galston@newcastle.ac.uk>
2019-11-07 10:23:25 +0000

    app can generate svg or json from canvas image

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,8 @@ import { Router } from '@angular/router';
 import { UUID } from 'angular2-uuid';
 import { Screenshot } from '@ionic-native/screenshot/ngx';
 import { Platform } from '@ionic/angular';
-import { fabric } from 'fabric';
+import { fabric} from 'fabric';
+import { StorageService } from '../storage.service';
 
 @Component({
   selector: 'app-home',
@@ -17,20 +18,43 @@ export class HomePage implements OnInit {
   screen: any;
 
   constructor(
+      private service: StorageService,
       private router: Router,
       @Inject(Screenshot) private screenshot: Screenshot,
       private platform: Platform) {}
 
   ngOnInit(): void {
     this.canvas = new fabric.Canvas('myCanvas');
-    this.canvas.setBackgroundImage('./assets/image.png', this.canvas.renderAll.bind(this.canvas));
+ //   this.canvas.setBackgroundImage('./assets/image.png', this.canvas.renderAll.bind(this.canvas));
+
+
+    console.log(this.canvas.width);
+    this.clear();
+    this.canvas.renderAll.bind(this.canvas);
   }
 
-  clear() {
+  async clear() {
     this.canvas.clear();
-    this.canvas.setBackgroundImage('./assets/image.png', this.canvas.renderAll.bind(this.canvas));
+   /*
+   scales background image to the size of the div, but it doesn't load correctly, only when you attempt to draw
+   const canvasHeight = this.canvas.height;
+    const canvasWidth = this.canvas.width;
+    const bgImg = new fabric.Image();
+    bgImg.setSrc('./assets/image.png', function() {
+        bgImg.set({
+            scaleX: canvasWidth / bgImg.width,
+            scaleY: canvasHeight / bgImg.height
+        });
+
+    });
+
+    this.canvas.setBackgroundImage(bgImg, this.canvas.renderAll.bind(this.canvas));
+    this.canvas.isDrawingMode = true;*/
+    this.canvas.setBackgroundImage('./assets/image4.png', this.canvas.renderAll.bind(this.canvas));
+    this.canvas.isDrawingMode = true;
   }
 
+  // This is now default on as listed above in clear and oninit
   enableDraw() {
     this.canvas.isDrawingMode = true;
   }
@@ -50,7 +74,8 @@ export class HomePage implements OnInit {
   }
 
   feedbackClicked() {
-    this.saveScreenShot().then( () => this.router.navigate(['/feedback/' + this.uniqueID]));
+   // this.saveScreenShot().then( () => this.router.navigate(['/feedback/' + this.uniqueID]));
+   this.saveSvg();
   }
 
   screenshotCounter() {
@@ -61,21 +86,56 @@ export class HomePage implements OnInit {
   }
 
   async saveScreenShot() {
-    this.uniqueID = UUID.UUID();
-    console.log(this.uniqueID);
-    await this.screenshot.save('jpg', 80, this.uniqueID).then(res => {
-    this.screen = res.filePath;
-    this.state = true;
-    console.log(this.screen);
-    console.log(this.state);
-    this.screenshotCounter();
-    return this.uniqueID;
-  });
+      console.log('export image');
+
+      this.uniqueID = UUID.UUID();
+      console.log(this.uniqueID);
+      await this.screenshot.save('jpg', 80, this.uniqueID).then(res => {
+      this.screen = res.filePath;
+      this.state = true;
+      console.log(this.screen);
+      console.log(this.state);
+      this.screenshotCounter();
+      return this.uniqueID;
+    });
 }
+
+
+  saveSvg() {
+    const toSVG = this.canvas.toSVG();
+    this.service.sendSVG(toSVG);
+  }
+
+  saveJson() {
+    let json_data = JSON.stringify(this.canvas.toDatalessJSON());
+    this.service.sendJSON(json_data);
+  }
+
+  downLoadJpg() {
+    const canvasDataUrl = this.canvas.toDataURL()
+    .replace(/^data:image\/[^;]*/, 'data:application/octet-stream'),
+    link = document.createElement('a'); // create an anchor tag
+
+    // set parameters for downloading
+    link.setAttribute('href', canvasDataUrl);
+    link.setAttribute('target', '_blank');
+    link.setAttribute('download', 'test5.png');
+
+    // compat mode for dispatching click on your anchor
+    if (document.createEvent) {
+    const evtObj = document.createEvent('MouseEvents');
+    evtObj.initEvent('click', true, true);
+    link.dispatchEvent(evtObj);
+    } else if (link.click) {
+    link.click();
+    }
+
+  }
 
   resetClicked() {
     // save screenshot
-    this.saveScreenShot().then( () => this.router.navigate(['/home']));
+    //this.saveScreenShot().then( () => this.router.navigate(['/home']));
+    this.saveJson();
   }
 
 }
